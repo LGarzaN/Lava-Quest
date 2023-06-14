c0c600036ca8685c22112d7f956fdd3ef8263f6d
Ramón Gómez <88677950+RamonGomezG@users.noreply.github.com>
2023-06-13 18:54:03 -0600

    Add files via upload

---
+++ LavaQuestDBAPI/LavaQuestDBAPI.Data/Repositories/iResultadosRepository.cs
@@ -0,0 +1,17 @@
+﻿using LavaQuestDBAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LavaQuestDBAPI.Data.Repositories
+{
+    public interface iResultadosRepository
+    {
+            Task<Resultados> GetResultados(string id);
+            Task<bool> InsertResultados(Resultados resultados);
+    }
+}
+
+
---
+++ LavaQuestDBAPI/LavaQuestDBAPI.Data/Repositories/iUsuariosRepository.cs
@@ -0,0 +1,19 @@
+﻿using LavaQuestDBAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LavaQuestDBAPI.Data.Repositories
+{
+    public interface iUsuariosRepository
+    {
+        Task<IEnumerable<Usuarios>> GetAllUsuarios();
+        Task<Usuarios> GetDetails(string correo);
+        Task<bool> InsertUsuario(Usuarios usuario);
+        Task<bool> UpdateUsuario(Usuarios usuario);
+        Task<bool> DeleteUsuario(Usuarios usuario);
+
+    }
+}
