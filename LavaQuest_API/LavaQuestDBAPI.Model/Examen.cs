490eddbe63ebfc3711d87d8170a654b3a971ee35
Ramón Gómez <88677950+RamonGomezG@users.noreply.github.com>
2023-06-13 18:49:20 -0600

    Add files via upload

---
+++ LavaQuest_API/LavaQuestDBAPI.Model/Examen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LavaQuestDBAPI.Model
+{
+    public class Examen
+    {
+        public int idExamen { get; set; }
+        public int idUsuario { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
+ 
---
+++ LavaQuest_API/LavaQuestDBAPI.Model/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LavaQuestDBAPI.Model
+{
+    public class Usuarios
+    {
+        public int idUsuarios { get; set; }
+        public int Tipo { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
+
