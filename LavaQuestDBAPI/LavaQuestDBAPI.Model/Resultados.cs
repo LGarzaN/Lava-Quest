aa2a66e094a9b5da876090d82a17b5257588b778
Ramón Gómez <88677950+RamonGomezG@users.noreply.github.com>
2023-06-13 18:55:22 -0600

    Add files via upload

---
+++ LavaQuestDBAPI/LavaQuestDBAPI.Model/Resultados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LavaQuestDBAPI.Model
+{
+    public class Resultados
+    {
+        public int idUsuario { get; set; } 
+        public int idExamen { get; set; }
+        public float Puntaje { get; set; }  
+
+    }
+}
